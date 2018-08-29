8db5fa3ff89ebaadb710ba02ecba71aff8772fff
wqferr <wqferr@gmail.com>
2018-08-29 13:50:46 -0300

    Add doc to FSM and remove knownStates

--- Assets/Design Patterns/FiniteStateMachine/FiniteStateMachine.cs
+++ Assets/Design Patterns/FiniteStateMachine/FiniteStateMachine.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 namespace HexCasters.DesignPatterns.FSM
 {
+	/// <summary>
+	/// A component implementing a finite state machine.
+	/// All states must be added to the same object as the machine component.
+	/// </summary>
 	public class FiniteStateMachine : MonoBehaviour
 	{
 		[SerializeField]
+		[Tooltip("The current state of the machine")]
 		private FsmState _state;
-		private ISet<FsmState> knownStates;
+
+		/// <summary>
+		/// The current state of the machine.
+		/// Trying to access this value before calling StartMachine will
+		/// result in an InvalidOperationException.
+		/// </summary>
+		/// <value>The current state of the machine.</value>
 		public FsmState State
 		{
 			get
@@ -19,39 +30,65 @@ namespace HexCasters.DesignPatterns.FSM
 			private set { this._state = value; }
 		}
 
+		/// <summary>
+		/// Whether or not the machine has been started.
+		/// </summary>
+		/// <value>true if StartMachine was called, false otherwise.</value>
 		public bool Started
 		{
 			get { return this._state != null; }
 		}
 
+		/// <summary>
+		/// Starts the machine with a given initial state.
+		/// Prior to calling this, all states the machine might transition to
+		/// must have been added to the same game object as the machine.
+		/// </summary>
+		/// <param name="initialState">The initial state of the machine.</param>
 		public void StartMachine(FsmState initialState)
 		{
-			this.knownStates = new HashSet<FsmState>();
 			foreach (var state in GetComponents<FsmState>())
 				AcknowledgeState(state);
-			ErrorIfUnknownState(initialState);
 			EnterState(initialState);
 		}
 
 		void AcknowledgeState(FsmState state)
 		{
-			this.knownStates.Add(state);
 			state.enabled = false;
 		}
 
+		/// <summary>
+		/// Starts the machine with an initial state of type T.
+		/// The machine looks for a component of type T in its game object,
+		/// and starts itself with it.
+		/// </summary>
+		/// <typeparam name="T">The type of the initial state.</typeparam>
 		public void StartMachine<T>() where T : FsmState
 		{
 			StartMachine(GetComponent<T>());
 		}
 
+		/// <summary>
+		/// Transition to a given state.
+		/// Calls Exit() on the current state, and Enter() on the next state,
+		/// in that order.
+		/// </summary>
+		/// <param name="nextState">The new state of the machine.</param>
 		public void Transition(FsmState nextState)
 		{
 			ErrorIfNotInitialized();
-			ErrorIfUnknownState(nextState);
 			ExitState();
 			EnterState(nextState);
 		}
 
+		/// <summary>
+		/// Transition to a state of the given type.
+		/// The machine looks for a component of type T in its game object, and
+		/// transitions to it.
+		/// Calls Exit() on the current state, and Enter() on the next state,
+		/// in that order.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
 		public void Transition<T>() where T : FsmState
 		{
 			Transition(GetComponent<T>());
@@ -79,12 +116,6 @@ namespace HexCasters.DesignPatterns.FSM
 		{
 			if (!this.Started)
 				throw new InvalidOperationException("FSM was not initialized");
-		}
-
-		private void ErrorIfUnknownState(FsmState state)
-		{
-			if (!knownStates.Contains(state))
-				throw new ArgumentException("Tried to switch to unkown state");
 		}
 
 		private void ErrorIfNullArgument(object value, string paramName)
