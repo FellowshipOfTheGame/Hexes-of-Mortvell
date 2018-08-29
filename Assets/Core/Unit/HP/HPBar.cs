349267c23430a349bf99d2d51b2d217dca345e19
wqferr <wqferr@gmail.com>
2018-08-29 12:43:09 -0300

    Create HPBar component

---
+++ Assets/Core/Unit/HP/HPBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using HexCasters.DesignPatterns.Observable;
+
+namespace HexCasters.Core.Units
+{
+	public class HPBar : MonoBehaviour
+	{
+		public HP hp;
+		public Transform barTransform;
+
+		private IObserver<int> handler;
+		private IDisposable subscription;
+
+		void Awake()
+		{
+			handler = new ValueObserver<int>(
+				nextEventHandler: UpdateBarLength);
+		}
+
+		void Start()
+		{
+			this.subscription = hp.CurrentAsObservable.Subscribe(handler);
+		}
+
+		void Destroy()
+		{
+			subscription.Dispose();
+		}
+
+		void UpdateBarLength(int newValue)
+		{
+			barTransform.localScale = (float) newValue / hp.max * Vector2.right;
+		}
+	}
+}
