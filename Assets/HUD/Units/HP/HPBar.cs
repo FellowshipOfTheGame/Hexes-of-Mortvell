d028c13b90e610e4b05261576385e9d482f14032
wqferr <wqferr@gmail.com>
2018-08-29 13:35:12 -0300

    Move HPBar out of core namespace

---
+++ Assets/HUD/Units/HP/HPBar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using HexCasters.DesignPatterns.Observable;
+using HexCasters.Core.Units;
+
+public class HPBar : MonoBehaviour
+{
+	public HP hp;
+	public Transform barTransform;
+
+	private IObserver<int> handler;
+	private IDisposable subscription;
+
+	void Awake()
+	{
+		handler = new ValueObserver<int>(
+			nextEventHandler: UpdateBarLength);
+	}
+
+	void Start()
+	{
+		this.subscription = hp.CurrentAsObservable.Subscribe(handler);
+		UpdateBarLength(hp.Current);
+	}
+
+	void Destroy()
+	{
+		subscription.Dispose();
+	}
+
+	void UpdateBarLength(int newValue)
+	{
+		barTransform.localScale = (float) newValue / hp.max * Vector2.right;
+	}
+}
