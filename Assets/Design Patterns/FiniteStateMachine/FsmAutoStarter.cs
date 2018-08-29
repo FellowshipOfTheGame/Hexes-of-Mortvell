5cf63f46ccd02aabb78387f01492cc42747ed38c
wqferr <wqferr@gmail.com>
2018-08-29 13:52:52 -0300

    Add XmlDoc to FsmAutoStarter

--- Assets/Design Patterns/FiniteStateMachine/FsmAutoStarter.cs
+++ Assets/Design Patterns/FiniteStateMachine/FsmAutoStarter.cs
@@ -2,9 +2,14 @@
 
 namespace HexCasters.DesignPatterns.FSM
 {
+	/// <summary>
+	/// Automatically starts an FSM on Start().
+	/// </summary>
 	public class FsmAutoStarter : MonoBehaviour
 	{
+		[Tooltip("The finite state machine to start")]
 		public FiniteStateMachine fsm;
+		[Tooltip("The state with which to start the machin")]
 		public FsmState initialState;
 
 		void Start()
