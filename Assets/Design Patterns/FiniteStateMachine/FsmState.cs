de205955dae1807dd9c7078179bd1ab206591da7
wqferr <wqferr@gmail.com>
2018-08-29 13:55:03 -0300

    Add XmlDoc to FsmState

--- Assets/Design Patterns/FiniteStateMachine/FsmState.cs
+++ Assets/Design Patterns/FiniteStateMachine/FsmState.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 namespace HexCasters.DesignPatterns.FSM
 {
+	/// <summary>
+	/// A state for a finite state machine.
+	/// </summary>
 	public class FsmState : MonoBehaviour
 	{
+		/// <summary>
+		/// A reference to this state's FSM.
+		/// </summary>
 		protected FiniteStateMachine fsm;
 
+		// Protected to avoid overrides or overshadowing
 		protected void Awake()
 		{
 			fsm = GetComponent<FiniteStateMachine>();
@@ -14,12 +21,20 @@ namespace HexCasters.DesignPatterns.FSM
 			enabled = false;
 		}
 
+		// Protected to avoid overrides or overshadowing
 		protected void Reset()
 		{
 			enabled = false;
 		}
 
+		/// <summary>
+		/// Called when the FSM transitions into this state.
+		/// </summary>
 		public virtual void Enter() {}
+
+		/// <summary>
+		/// Called when the FSM transitions out of this state.
+		/// </summary>
 		public virtual void Exit() {}
 
 		void ErrorIfNoFsmAttached()
