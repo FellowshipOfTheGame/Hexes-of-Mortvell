f64c70dfdbe3bd97d89c2485af00d4251465046c
wqferr <wqferr@gmail.com>
2018-08-31 09:23:05 -0300

    Document Teams namespace stuff

--- Assets/Core/Units/Teams/Team.cs
+++ Assets/Core/Units/Teams/Team.cs
@@ -5,10 +5,21 @@ using System.Collections.ObjectModel;
 
 namespace HexCasters.Core.Units.Teams
 {
+	/// <summary>
+	/// A group of objects with the TeamMember component.
+	/// </summary>
 	public class Team : MonoBehaviour
 	{
 		[SerializeField]
 		private List<TeamMember> members;
+
+		/// <summary>
+		/// An IReadOnlyList of the team's current members.
+		/// </summary>
+		/// <remarks>
+		/// This is a costly operation and should not be called in a loop
+		/// unless the team is actively being modified.
+		/// </remarks>
 		public IReadOnlyList<TeamMember> Members
 		{
 			get { return new ReadOnlyCollection<TeamMember>(members); }
@@ -19,6 +30,24 @@ namespace HexCasters.Core.Units.Teams
 			this.members = new List<TeamMember>();
 		}
 
+		/// <summary>
+		/// Add a GameObject to the team.
+		/// </summary>
+		/// <param name="gameObject">The GameObject to add to the team.</param>
+		/// <remarks>
+		/// <para>
+		/// If the GameObject does not have a TeamMember component, a new one
+		/// will be attached to it.
+		/// </para>
+		/// <para>
+		/// If the GameObject has a TeamMember component and its team is null,
+		/// its team property will be set to the object's newly assigned team.
+		/// </para>
+		/// <para>
+		/// If the GameObject has a TeamMember component and its team is not null,
+		/// an ArgumentException will be thrown.
+		/// </para>
+		/// </remarks>
 		public void Add(GameObject gameObject)
 		{
 			var teamMembership = GetOrCreateTeamMembership(gameObject);
@@ -27,11 +56,29 @@ namespace HexCasters.Core.Units.Teams
 			this.members.Add(teamMembership);
 		}
 
+		/// <summary>
+		/// Removes a GameObject from the team.
+		/// </summary>
+		/// <param name="teamMembership">The TeamMember component of the object to be removed.</param>
+		/// <remarks>
+		/// <para>
+		/// If teamMembership is null or its team is null, an ArgumentException
+		/// will be thrown.
+		/// </para>
+		/// <para>
+		/// If the object belongs to another team, an ArgumentException will be
+		/// thrown.
+		/// </para>
+		/// <para>
+		/// After removal, the TeamMember component will be removed.
+		/// </para>
+		/// </remarks>
 		public void Remove(TeamMember teamMembership)
 		{
+			ErrorIfNullTeam(teamMembership);
 			ErrorIfMemberOfOtherTeam(teamMembership);
 			this.members.Remove(teamMembership);
-			teamMembership.team = null;
+			Destroy(teamMembership);
 		}
 
 		TeamMember GetOrCreateTeamMembership(GameObject gameObject)
@@ -49,6 +96,12 @@ namespace HexCasters.Core.Units.Teams
 			var objName = teamMembership.gameObject.name;
 			throw new ArgumentException(
 				$"Object \"{objName}\"is already a member of another team");
+		}
+
+		void ErrorIfNullTeam(TeamMember teamMembership)
+		{
+			if (teamMembership?.team == null)
+				throw new ArgumentException("Object has no team");
 		}
 	}
 }
--- Assets/Core/Units/Teams/TeamMember.cs
+++ Assets/Core/Units/Teams/TeamMember.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 namespace HexCasters.Core.Units.Teams
 {
+	/// <summary>
+	/// Indicates this GameObject is part of a team.
+	/// </summary>
 	public class TeamMember : MonoBehaviour
 	{
+		/// <summary>
+		/// The team the object currently belongs to.
+		/// </summary>
+		[Tooltip("The team the object belongs to.")]
 		public Team team;
 
 		void Start()
@@ -16,6 +23,12 @@ namespace HexCasters.Core.Units.Teams
 			ExitTeam();
 		}
 
+		/// <summary>
+		/// Removes the object from the current team.
+		/// </summary>
+		/// <remarks>
+		/// Exiting a team destroys the TeamMember component.
+		/// </remarks>
 		public void ExitTeam()
 		{
 			this.team.Remove(this);
