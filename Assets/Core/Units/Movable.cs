6c0faa8fe81bef5650703eed5babd842ffffbcd7
wqferr <wqferr@gmail.com>
2018-08-31 09:28:22 -0300

    Document Movable

--- Assets/Core/Units/Movable.cs
+++ Assets/Core/Units/Movable.cs
@@ -5,19 +5,46 @@ using HexCasters.Core.Grid.Regions;
 
 namespace HexCasters.Core.Units
 {
+	/// <summary>
+	/// Component of GameObjects that can move on the board.
+	/// </summary>
+	[RequireComponent(typeof(BoardCellContent))]
 	public class Movable : MonoBehaviour
 	{
+		/// <summary>
+		/// Amount of points the GameObject can spend per movement.
+		/// </summary>
+		[Tooltip("Amount of points the GameObject can spend per movement")]
 		public int movementPoints;
 
-		public IEnumerable<BoardCell> FindReachableRegion(
-			BoardCell currentCell)
+		private BoardCellContent asCellContent;
+
+		void Awake()
 		{
-			return currentCell.Neighborhood(
-				this.movementPoints,
-				DestinationMovementCost);
+			this.asCellContent = GetComponent<BoardCellContent>();
 		}
 
-		private static int DestinationMovementCost(BoardCell from, BoardCell to)
+		/// <summary>
+		/// Enumerates over all cells the GameObject can reach.
+		/// </summary>
+		/// <param name="currentCell">The current cell of the GameObject.</param>
+		/// <remarks>
+		/// The returned cells are the Neighborhood of the current cell of the
+		/// object. The distance function used is the movement cost of the
+		/// destination cell, and the maximum distance from the origin is the
+		/// number of movement points of this object.
+		/// </remarks>
+		public IEnumerable<BoardCell> FindReachableRegion()
+		{
+			var currentCell = this.asCellContent.Cell;
+			return currentCell.Neighborhood(
+				this.movementPoints,
+				DestinationMovementCostAsDistance);
+		}
+
+		private static int DestinationMovementCostAsDistance(
+			BoardCell from,
+			BoardCell to)
 		{
 			return to.Terrain.movementCost;
 		}
