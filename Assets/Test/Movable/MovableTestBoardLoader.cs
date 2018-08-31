bc079a7b4884fbf15d99467fe1e1fe7b9c93dbcc
wqferr <wqferr@gmail.com>
2018-08-30 23:01:31 -0300

    Start MovableTest

---
+++ Assets/Test/Movable/MovableTestBoardLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using HexCasters.Core.Units;
+using HexCasters.Core.Grid;
+using HexCasters.Core.Grid.Regions;
+
+public class MovableTestBoardLoader : MonoBehaviour
+{
+	public Board board;
+	public BoardLayout layout;
+	public GameObject unitPrefab;
+	public GameObject highlightPrefab;
+	private BoardCellContent unitInstanceCellContent;
+
+	void Start()
+	{
+		board.LoadLayout(layout);
+		this.unitInstanceCellContent = this.board.Spawn(
+			unitPrefab,
+			new BoardPosition(-2, 3));
+		ShowMoveArea();
+	}
+
+	void ShowMoveArea()
+	{
+		var unitCell = this.unitInstanceCellContent.Cell;
+		var unitMovable = this.unitInstanceCellContent.GetComponent<Movable>();
+		var area = unitCell.Neighborhood(unitMovable.movementPoints);
+		foreach (var cell in area)
+		{
+			var highlightInstance = Instantiate(
+				highlightPrefab,
+				cell.transform,
+				false);
+		}
+	}
+}
