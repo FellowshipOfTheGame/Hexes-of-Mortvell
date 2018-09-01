a9159aad8b4a6b5c5fca8b31bbc55fb64d8e95a6
wqferr <wqferr@gmail.com>
2018-09-01 15:52:13 -0300

    Make Actor a flag Component

--- Assets/Core/Actions/Actor.cs
+++ Assets/Core/Actions/Actor.cs
@@ -6,29 +6,5 @@ namespace HexCasters.Core.Actions
 {
 	[DisallowMultipleComponent]
 	[RequireComponent(typeof(BoardCellContent))]
-	public class Actor : MonoBehaviour
-	{
-		[SerializeField]
-		private List<GameObject> knownActions;
-		public Dictionary<string, GameObject> actionsByName;
-
-		void Awake()
-		{
-			this.actionsByName = new Dictionary<string, GameObject>();
-			foreach (var action in this.knownActions)
-				this.actionsByName[action.name] = action;
-		}
-
-		public bool KnowsAction(string actionName)
-		{
-			return this.actionsByName.ContainsKey(actionName);
-		}
-
-		void ErrorIfUnknownAction(string actionName)
-		{
-			if (!KnowsAction(actionName))
-				throw new System.ArgumentException(
-					$"Unknown action: {actionName}");
-		}
-	}
+	public class Actor : MonoBehaviour {}
 }
