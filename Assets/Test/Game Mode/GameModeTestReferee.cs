00d602274f9cd8045678deb0d61452e32137de14
wqferr <wqferr@gmail.com>
2018-09-01 14:12:13 -0300

    Divide referee component in Game Mode test

--- Assets/Test/Game Mode/GameModeTestReferee.cs
+++ Assets/Test/Game Mode/GameModeTestReferee.cs
@@ -1,52 +1,16 @@
-﻿using UnityEngine;
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using HexCasters.Core.Units.Teams;
-using HexCasters.DesignPatterns.Observer;
-using HexCasters.DesignPatterns.Fsm;
+using UnityEngine;
 
 namespace HexCasters.Testing.GameModeTest
 {
 	public class GameModeTestReferee : MonoBehaviour
 	{
-		public List<Team> teams;
 		public GameModeTestTurn turn;
-		public FiniteStateMachine fsm;
+		public GameObject thingy;
 
-		List<IDisposable> observableTeamSubscriptions;
-
-		void Start()
+		public void AwardVictoryToCurrentTeam()
 		{
-			var observableTeamMemberLists = teams
-				.Select(team => team.AsObservable)
-				.ToList();
-			var teamChangedHandler = new ValueObserver<IList<TeamMember>>(
-				nextEventHandler: TeamChangedEvent);
-			this.observableTeamSubscriptions = new List<IDisposable>();
-			foreach (var memberList in observableTeamMemberLists)
-			{
-				var subscription = memberList.Subscribe(teamChangedHandler);
-				this.observableTeamSubscriptions.Add(subscription);
-			}
-		}
-
-		void OnDestroy()
-		{
-			foreach (var subscription in this.observableTeamSubscriptions)
-				subscription.Dispose();
-		}
-
-		void TeamChangedEvent(IList<TeamMember> members)
-		{
-			if (members.Count == 0)
-				EndGame();
-		}
-
-		void EndGame()
-		{
-			Debug.Log(turn.CurrentTeam);
-			this.fsm.Transition<GameModeTestEndGameState>();
+			thingy.SetActive(true);
+			Debug.Log(turn.CurrentTeam.name);
 		}
 	}
 }
---
+++ Assets/Test/Game Mode/GameModeTestTeamSizeChangeListener.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using HexCasters.Core.Units.Teams;
+using HexCasters.DesignPatterns.Observer;
+
+namespace HexCasters.Testing.GameModeTest
+{
+	public class GameModeTestTeamSizeChangeListener : MonoBehaviour
+	{
+		public Team team;
+		public GameModeTestReferee referee;
+		IDisposable observableTeamSubscription;
+
+		void Start()
+		{
+			var teamChangedHandler = new ValueObserver<IList<TeamMember>>(
+				nextEventHandler: TeamMembersChangedEvent);
+			var observableTeam = this.team.AsObservable;
+			this.observableTeamSubscription = observableTeam
+				.Subscribe(teamChangedHandler);
+		}
+
+		void OnDestroy()
+		{
+			this.observableTeamSubscription.Dispose();
+		}
+
+		void TeamMembersChangedEvent(IList<TeamMember> teamMembers)
+		{
+			if (teamMembers.Count == 0)
+				referee.AwardVictoryToCurrentTeam();
+		}
+	}
+}
