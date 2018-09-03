eada0b0ae46da97e51bb8a49675e7fc9d65590b4
wqferr <wqferr@gmail.com>
2018-09-03 19:11:58 -0300

    Create radius target filter

---
+++ Assets/Game/Game Modes/Common/Action Components/Target Filters/RadiusTargetFilter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using HexCasters.Core.Actions;
+using HexCasters.Core.Grid;
+using HexCasters.Core.Grid.Regions;
+
+namespace HexCasters.GameModes.Common
+{
+	public class RadiusTargetFilter : ActionTargetFilter
+	{
+		public int radius;
+
+		public override IEnumerable<BoardCell> ValidTargets(
+			BoardCellContent actor,
+			IEnumerable<BoardCell> partialTargets)
+		{
+			return actor.Cell.Neighborhood(this.radius);
+		}
+	}
+}
