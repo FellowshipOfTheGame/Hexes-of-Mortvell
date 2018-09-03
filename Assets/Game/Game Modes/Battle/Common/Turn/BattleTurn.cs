ee851812da4bd56b910d85287ac8038a484c50be
wqferr <wqferr@gmail.com>
2018-09-03 14:13:32 -0300

    Create turn script for battle

---
+++ Assets/Game/Game Modes/Battle/Common/Turn/BattleTurn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using HexCasters.Core.Units.Teams;
+
+namespace HexCasters.GameModes.Battle.Common
+{
+	public class BattleTurn : MonoBehaviour
+	{
+		[Tooltip("Teams which have turns to act.")]
+		public List<Team> teamsWithTurns;
+		private int currentTeamIndex;
+
+		public Team CurrentTeam
+		{
+			get { return this.teamsWithTurns[this.currentTeamIndex]; }
+		}
+
+		public void NextTeam()
+		{
+			this.currentTeamIndex++;
+			this.currentTeamIndex %= this.teamsWithTurns.Count;
+		}
+	}
+}
