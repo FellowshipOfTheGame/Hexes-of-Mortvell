66d6b60e4e8a599b2ebfce0f3be942494fab0d03
wqferr <wqferr@gmail.com>
2018-09-04 12:48:22 -0300

    Add perform action state

--- Assets/Game/Game Modes/Battle/Common/Turn/States/BattlePerformActionState.cs
+++ Assets/Game/Game Modes/Battle/Common/Turn/States/BattlePerformActionState.cs
@@ -1,4 +1,5 @@
 ﻿using HexCasters.DesignPatterns.Fsm;
+using HexCasters.Core.Actions;
 
 namespace HexCasters.GameModes.Battle.Common
 {
@@ -8,10 +9,22 @@ namespace HexCasters.GameModes.Battle.Common
 
 		public override void Enter()
 		{
+			PerformAction();
+			this.fsm.Transition<BattleOverviewState>();
 		}
 
-		public override void Exit()
+		public override void Exit() {}
+
+		void PerformAction()
 		{
+			var action = this.playerOrders.action;
+			var aoeComponent = action.GetComponent<ActionAoe>();
+			var aoe = aoeComponent.GetAoe(this.playerOrders.actionTargets);
+			var activeComponent= action.GetComponent<ActionActivation>();
+			activeComponent.Perform(
+				this.playerOrders.unit.AsCellContent,
+				this.playerOrders.actionTargets,
+				aoe);
 		}
 	}
 }
