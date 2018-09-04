525f906dd187c8799dc46ea3ec057cb97c3bc5ad
wqferr <wqferr@gmail.com>
2018-09-03 23:40:12 -0300

    Create cell click listener

---
+++ Assets/Game/Game Modes/Common/Cell/CellClickNotifyBoard.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using HexCasters.Core.Grid;
+
+namespace HexCasters.GameModes.Common
+{
+	public class CellClickNotifyBoard : MonoBehaviour
+	{
+		public BoardCell cell;
+		private CellClickListener listener;
+
+		void Awake()
+		{
+			var board = this.cell.board;
+			this.listener = board.GetComponent<CellClickListener>();
+		}
+
+		public void HoverEnter()
+		{
+			this.listener.Notify(this.cell);
+		}
+	}
+}
