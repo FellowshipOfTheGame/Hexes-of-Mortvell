4e38763987aa7e65f8f8e99d3aa7da4131b6d9da
wqferr <wqferr@gmail.com>
2018-09-03 21:36:20 -0300

    Add LineTowards with Direction param

--- Assets/Game/Core/Grid/Regions/LineRegion.cs
+++ Assets/Game/Core/Grid/Regions/LineRegion.cs
@@ -4,7 +4,7 @@ namespace HexCasters.Core.Grid.Regions
 {
 	public static class BoardLineRegionExtension
 	{
-		public static IEnumerable<BoardCell> LineTo(
+		public static IEnumerable<BoardCell> StraightLineTowards(
 			this BoardCell origin,
 			BoardCell dest,
 			int? maxLength=null,
@@ -12,30 +12,54 @@ namespace HexCasters.Core.Grid.Regions
 		{
 			var nullableDir = origin.Position
 				.StraightLineDirectionTowards(dest.Position);
+			if (!nullableDir.HasValue)
+			{
+				var list = new List<BoardCell>();
+				if (includeOrigin)
+					list.Add(origin);
+				return list;
+			}
+
+			var direction = nullableDir.Value;
+			var distToCell = origin.Position
+				.ManhattanDistanceTo(dest.Position);
+			if (!maxLength.HasValue || maxLength.Value > distToCell)
+				maxLength = distToCell;
+			return origin.StraightLineTowards(
+				direction,
+				maxLength: maxLength,
+				includeOrigin: includeOrigin);
+		}
+
+		public static IEnumerable<BoardCell> StraightLineTowards(
+			this BoardCell origin,
+			Direction direction,
+			int? maxLength=null,
+			bool includeOrigin=false)
+		{
 			var line = new List<BoardCell>();
 
 			if (includeOrigin)
-				line.Add(origin);
-			if (!nullableDir.HasValue)
-				return line;
+				yield return origin;
 
-			var direction = nullableDir.Value;
 			var iterCell = origin;
 			int distanceFromOrigin = 0;
 
 			do
 			{
 				iterCell = iterCell.FindAdjacentCell(direction);
+
+				// end of board
 				if (iterCell == null)
 					break;
-				line.Add(iterCell);
+				yield return iterCell;
 				distanceFromOrigin++;
-				if (maxLength.HasValue && distanceFromOrigin >= maxLength.Value)
-					break;
-			}
-			while (iterCell != dest);
 
-			return line;
+				// skip distance check if no limit
+				if (!maxLength.HasValue)
+					continue;
+			}
+			while (distanceFromOrigin < maxLength.Value);
 		}
 	}
 }
