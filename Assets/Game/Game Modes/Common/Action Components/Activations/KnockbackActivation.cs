20fe1307e8d59e00f6b51692589dd0d24a93e285
Marjorie <marjorieysaito@gmail.com>
2018-09-22 16:35:12 -0300

    Remove Queue Aoe

--- Assets/Game/Game Modes/Common/Action Components/Activations/KnockbackActivation.cs
+++ Assets/Game/Game Modes/Common/Action Components/Activations/KnockbackActivation.cs
@@ -6,25 +6,30 @@ namespace HexesOfMortvell.GameModes.Common
 {
 	public class KnockbackActivation : DamageActivation
 	{
+		private List<BoardCell> realAoe;
+
 		public override void Perform(
 			BoardCellContent actor,
 			IEnumerable<BoardCell> targets,
 			IEnumerable<BoardCell> aoe)
 		{
-      base.Perform(actor, targets, aoe);
       var targetList = new List<BoardCell>(targets);
+			realAoe = new List<BoardCell>(aoe);
       Direction direction = (Direction)DirectionCellExtensions.StraightLineDirectionTowards(
         actor.Cell.GetPosition(),
         targetList[0].GetPosition());
       BoardCell knockedBack = targetList[0].FindAdjacentCell(direction);
       if (knockedBack != null) {
-        if (knockedBack.GetContent() == null)
-          targetList[0].MoveContentTo(knockedBack);
+        if (knockedBack.GetContent() == null) {
+					realAoe.Add(knockedBack);
+					targetList[0].MoveContentTo(knockedBack);
+				}
       }
+	    base.Perform(actor, targets, realAoe);
 		}
 
 		public override void Cleanup(IEnumerable<BoardCell> aoe) {
-      base.Cleanup(aoe);
+      base.Cleanup(realAoe);
     }
 	}
 }
