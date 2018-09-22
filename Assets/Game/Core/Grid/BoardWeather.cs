7164bc6f3b04d8c68030d8dbdc393c4e053d2a1b
wqferr <wqferr@gmail.com>
2018-09-21 22:41:57 -0300

    Fix problem with weather priority

---
+++ Assets/Game/Core/Grid/BoardWeather.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace HexesOfMortvell.Core.Grid
+{
+	public class BoardWeather : MonoBehaviour
+	{
+		public string uniqueName;
+
+		public override bool Equals(object other)
+		{
+			if (base.Equals(other))
+				return true;
+			var asWeather = other as BoardWeather;
+			return this.uniqueName.Equals(asWeather?.uniqueName);
+		}
+
+		public override int GetHashCode()
+		{
+			return this.uniqueName.GetHashCode();
+		}
+	}
+}
