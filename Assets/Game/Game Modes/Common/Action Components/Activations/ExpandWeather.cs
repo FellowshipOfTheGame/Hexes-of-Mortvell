34e9cfad87620013fc27b8e52c2709f1f21c2f9a
wqferr <wqferr@gmail.com>
2018-10-08 00:08:13 -0300

    Recreate Call Winds spell prefab (unity bug probs)

---
+++ Assets/Game/Game Modes/Common/Action Components/Activations/ExpandWeather.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using HexesOfMortvell.Core.Actions;
+using HexesOfMortvell.Core.Grid;
+
+namespace HexesOfMortvell.GameModes.Common
+{
+	public class ExpandWeather : ActionActivation
+	{
+		public override void Perform(
+			BoardCellContent actor,
+			IEnumerable<BoardCell> targets,
+			IEnumerable<BoardCell> aoe)
+		{
+			var origin = targets.First();
+			foreach (var cell in aoe)
+				cell.ChangeWeather(origin.Weather.gameObject);
+		}
+
+		public override void Cleanup(IEnumerable<BoardCell> aoe) {}
+	}
+}
---
+++ Assets/Game/HUD/Actions/ActionIcon.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HexesOfMortvell.Hud.Actions
+{
+	public class ActionIcon : MonoBehaviour
+	{
+		public Sprite icon;
+	}
+}
