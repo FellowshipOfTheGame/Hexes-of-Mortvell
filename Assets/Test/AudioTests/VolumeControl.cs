7312051f809997044bc004fcf620fa516e5b51f8
billionai <larsen.bruno.p@gmail.com>
2018-10-21 09:20:52 -0300

    Added volume control script, and removed separated fire

---
+++ Assets/Test/AudioTests/VolumeControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    private AudioSource AS;
+    private float vol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+        vol = AS.volume;
+    }
+
+    void Update()
+    {
+        vol = AS.volume;
+    }
+
+    // Update is called once per frame
+    void OnAudioFilterRead(float[] Data, int channel)
+    {
+        for(int i=0;i<Data.Length; i++)
+        {
+            Data[i] *= vol;
+        }
+    }
+}
