8dc6849939182a0b6bbfc83a52b7ae7f6f61b7bf
billionai <larsen.bruno.p@gmail.com>
2018-10-29 21:25:08 -0300

    Added default first value, and slider now works on all menus

--- Assets/Game/Core/Music/MusicVolume.cs
+++ Assets/Game/Core/Music/MusicVolume.cs
@@ -7,9 +7,14 @@ public class MusicVolume : MonoBehaviour
 	private AudioSource AS;
     // Start is called before the first frame update
 	void Awake(){
+		//set the first value for music volume, if none was set before
 		var tmp = PlayerPrefs.GetFloat("BGM",-1f);
 		if(tmp == -1f)
 			PlayerPrefs.SetFloat("BGM",1f);
+		//set the first value for SFX Volume, if none was set before
+		tmp = PlayerPrefs.GetFloat("SFX", -1f);
+		if(tmp == -1f)
+			PlayerPrefs.SetFloat("SFX",1f);
 	}
     void Start()
     {
