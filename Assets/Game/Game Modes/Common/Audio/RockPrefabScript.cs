8021f8db02a69332b5c418648e330bf4a69e93eb
Unknown <larsen.bruno.p@gmail.com>
2018-11-11 15:33:01 -0200

    Working prefab for rock sound!

---
+++ Assets/Game/Game Modes/Common/Audio/RockPrefabScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPrefabScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var windSound = GetComponent<TimedWhiteNoise>();
+		windSound.startSound();
+    }
+
+	public void colide(){
+		var colSound = GetComponent<RockColision>();
+		colSound.play();
+		Destroy(this.gameObject, 0.02f);
+	}
+}
