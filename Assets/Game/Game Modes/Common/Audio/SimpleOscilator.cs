ac6b68fdb3e460473414c1b21a41d0a8ecc7a34a
Unknown <larsen.bruno.p@gmail.com>
2018-12-07 16:59:59 -0200

    Improved envelope

--- Assets/Game/Game Modes/Common/Audio/SimpleOscilator.cs
+++ Assets/Game/Game Modes/Common/Audio/SimpleOscilator.cs
@@ -9,29 +9,40 @@ public class SimpleOscilator : MonoBehaviour
 	public float perc;
 	
 	private float constant;
+	private float beating;
 	private int state;
     // Start is called before the first frame update
     void Awake()
     {
         constant = 40 * Mathf.PI * frequency / 1000;
+		beating = 39 * Mathf.PI * frequency / 1000;
     }
 
     // Update is called once per frame
     void OnAudioFilterRead(float[] data, int channels)
     {
         float norm;
-		if(state > 0){
+		if(state > 1){
 			for(int i=0; i<data.Length/channels; i++){
 				norm = i;
 				norm /= data.Length/channels;
 				for(int j = 0; j<channels; j++){
-					data[channels*i + j] = Mathf.Cos(norm * constant)*perc + data[channels*i + j]*(1-perc);
+					data[channels*i + j] = Mathf.Cos(norm * constant)*perc/2 + Mathf.Cos(norm* beating)*perc/2 + data[channels*i + j]*(1-perc);
 				}
 			}
 			for(int i=20; i<data.Length; i++){
 				data[i] = 0.75f*data[i] + 0.125f*data[i-10] + 0.125f*data[i-20];
 			}
 			state--;
+		}else if(state > maxState / 10){
+			for(int i = 0; i<data.Length/channels; i++){
+				norm = i;
+				norm /= data.Length/channels;
+				for(int j=0; j<channels; j++){
+					data[channels*i + j] = Mathf.Cos(norm * constant) * perc/2 * (1-norm) + Mathf.Cos(norm * beating) * perc/2 * (1-norm) + data[channels*i + j]*(1-perc);
+				}
+			}
+			state --;
 		}
     }
 	
