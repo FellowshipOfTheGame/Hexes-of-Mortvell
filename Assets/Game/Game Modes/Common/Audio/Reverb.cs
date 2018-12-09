f1336235173bcfd53faa1e737028a7de6012bcfe
Unknown <larsen.bruno.p@gmail.com>
2018-12-09 13:12:11 -0200

    added reverb to make sound less dry (?)

---
+++ Assets/Game/Game Modes/Common/Audio/Reverb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reverb : MonoBehaviour
+{
+
+	public int delay;
+	public float amplitude;
+	
+	private float[] last;
+    void Awake()
+    {
+        last = new float[delay];
+    }
+
+    
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        for(int i=0;i<delay;i++){
+			data[i] = (1-amplitude)*data[i] + amplitude*last[i];
+		}
+		for(int i=delay; i<data.Length; i++){
+			data[i] = (1-amplitude)*data[i] + amplitude*data[i - delay];
+		}
+		for(int i = 0; i<delay; i++){
+			last[i] = data[data.Length - delay - 1 + i];
+		}
+    }
+}
--- Assets/Game/Game Modes/Common/Audio/SkeletonPrefabScript.cs
+++ Assets/Game/Game Modes/Common/Audio/SkeletonPrefabScript.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class SkeletonPrefabScript : MonoBehaviour
 {
-	public Crackle timedCrackle;
-	public float waitTime;
 	public float lifeLength;
-	
-	private float waitLeft;
     // Start is called before the first frame update
     void Start()
     {
-		var windSound = GetComponent<TimedWhiteNoise>();
-		windSound.startSound();
-        waitLeft = waitTime;
 		Destroy(gameObject, lifeLength);
     }
 
     // Update is called once per frame
     void Update()
     {
-        waitLeft -= Time.deltaTime;
-		if(waitLeft > 0){
-			timedCrackle.enabled = true;
-		}
     }
 }
