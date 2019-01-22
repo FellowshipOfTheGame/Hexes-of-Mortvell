350df379ad84f7ca8e3a975c40eb0561e8670d51
Unknown <marjorieysaito@gmail.com>
2019-01-22 18:42:20 -0200

    Start audio tests

---
+++ Assets/Test/AudioTests/InstantiateOnStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexesOfMortvell.Test
+{
+	public class InstantiateOnStart : MonoBehaviour
+	{
+		public GameObject prefab;
+		private TimedWhiteNoise whiteNoise;
+
+		void Start()
+		{
+			Instantiate(prefab);
+			whiteNoise = prefab.GetComponent<TimedWhiteNoise>();
+			whiteNoise.startSound();
+		}
+	}
+}
